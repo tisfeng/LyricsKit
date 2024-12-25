8cebb14ebbea57c6890aa383845d5f0d1218bb7d
tisfeng <tisfeng@gmail.com>
2024-12-25 18:02:20 +0800

    refactor: reorganize qq musics api, remove unused code

--- Sources/LyricsService/Provider/QQMusic.swift
+++ Sources/LyricsService/Provider/QQMusic.swift
@@ -12,16 +12,16 @@ import CXShim
 import Foundation
 import LyricsCore
 
-//import SWXMLHash
-
 #if canImport(FoundationNetworking)
     import FoundationNetworking
 #endif
 
-private let qqSearchBaseURLString1 = "https://c.y.qq.com/splcloud/fcgi-bin/smartbox_new.fcg"
+private let qqSearchBaseURLString = "https://c.y.qq.com/splcloud/fcgi-bin/smartbox_new.fcg"
+private let qqLyricsBaseURLString = "https://c.y.qq.com/qqmusic/fcgi-bin/lyric_download.fcg"
+
+// This lyrics result has no karaoke time info.
 private let qqSearchBaseURLString2 = "https://u.y.qq.com/cgi-bin/musicu.fcg"
-private let qqLyricsBaseURLString1 = "https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg"
-private let qqLyricsBaseURLString2 = "https://c.y.qq.com/qqmusic/fcgi-bin/lyric_download.fcg"
+private let qqLyricsBaseURLString2 = "https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg"
 
 extension LyricsProviders {
     public final class QQMusic {
@@ -34,11 +34,12 @@ extension LyricsProviders.QQMusic: _LyricsProvider {
 
     public static let service: LyricsProviders.Service? = .qq
 
+    /// Search song info by keyword.
     public func lyricsSearchPublisher(request: LyricsSearchRequest) -> AnyPublisher<
         LyricsToken, Never
     > {
         let parameter = ["key": request.searchTerm.description]
-        let url = URL(string: qqSearchBaseURLString1 + "?" + parameter.stringFromHttpParameters)!
+        let url = URL(string: qqSearchBaseURLString + "?" + parameter.stringFromHttpParameters)!
 
         return sharedURLSession.cx.dataTaskPublisher(for: url)
             .map { $0.data }
@@ -52,38 +53,7 @@ extension LyricsProviders.QQMusic: _LyricsProvider {
         //            .eraseToAnyPublisher()
     }
 
-    public func lyricsSearchPublisher2(request: LyricsSearchRequest) -> AnyPublisher<
-        LyricsToken, Never
-    > {
-        let parameter = [
-            "req_1": [
-                "method": "DoSearchForQQMusicDesktop",
-                "module": "music.search.SearchCgiService",
-                "param": [
-                    "num_per_page": "20",
-                    "page_num": "1",
-                    "query": request.searchTerm.titleOnly,
-                    "search_type": "0",
-                ],
-            ]
-        ]
-
-        let jsonData = try! JSONSerialization.data(withJSONObject: parameter, options: [])
-        let jsonString = String(data: jsonData, encoding: .utf8)!
-        let encodedJsonString = jsonString.addingPercentEncoding(
-            withAllowedCharacters: .urlQueryAllowed)!
-        let urlString = qqSearchBaseURLString2 + "?data=" + encodedJsonString
-        let url = URL(string: urlString)!
-        return sharedURLSession.cx.dataTaskPublisher(for: url)
-            .map { $0.data }
-            .decode(type: QQResponseSearchResult2.self, decoder: JSONDecoder().cx)
-            .map(\.request.data.body.song.list)
-            .replaceError(with: [])
-            .flatMap(Publishers.Sequence.init)
-            .map { $0 as LyricsToken }
-            .eraseToAnyPublisher()
-    }
-
+    /// Fetch lyrics by song id.
     public func lyricsFetchPublisher(token: LyricsToken) -> AnyPublisher<Lyrics, Never> {
         let parameter: [String: Any] = [
             "musicid": token.id,
@@ -91,7 +61,7 @@ extension LyricsProviders.QQMusic: _LyricsProvider {
             "miniversion": 82,
             "lrctype": 4,
         ]
-        let url = URL(string: qqLyricsBaseURLString2 + "?" + parameter.stringFromHttpParameters)!
+        let url = URL(string: qqLyricsBaseURLString + "?" + parameter.stringFromHttpParameters)!
         var req = URLRequest(url: url)
         req.setValue("y.qq.com/portal/player.html", forHTTPHeaderField: "Referer")
         return sharedURLSession.cx.dataTaskPublisher(for: req)
@@ -102,31 +72,16 @@ extension LyricsProviders.QQMusic: _LyricsProvider {
                 dataString = dataString.replacingOccurrences(of: "<!--", with: "")
                 dataString = dataString.replacingOccurrences(of: "-->", with: "")
                 guard let xmlDocument = try? XMLUtils.create(content: dataString),
-                    let encryptedString = try? xmlDocument.nodes(forXPath: "//content").first?
-                        .stringValue,
+                    let encryptedString = try? xmlDocument.nodes(forXPath: "//content").first?.stringValue,
                     let decryptedString = decryptQQMusicQrc(encryptedString),
-                    //                      let decryptedXMLDocument = try? XMLDocument(xmlString: decryptedString, options: [.nodePreserveAll, .nodeNeverEscapeContents]),
-                    //                      let qrcContent = try? decryptedXMLDocument.nodes(forXPath: "//@LyricContent").first?.xmlString(options: .nodePreserveAll),
                     let lrc = Lyrics(qqmusicQrcContent: decryptedString)
                 else {
                     return nil
                 }
 
-                //                guard let model = try? JSONDecoder().decode(QQResponseSingleLyrics.self, from: data),
-                //                    let lrcContent = model.lyricString,
-                //                    let lrc = Lyrics(lrcContent) else {
-                //                        return nil
-                //                }
-                //                if let transLrcContent = model.transString,
-                //                    let transLrc = Lyrics(transLrcContent) {
-                //                    lrc.merge(translation: transLrc)
-                //                }
-
                 lrc.idTags[.title] = token.name
                 lrc.idTags[.artist] = token.singers.joined(separator: ",")
-                //                lrc.idTags[.album] = token.albumname
 
-                //                lrc.length = Double(token.interval)
                 lrc.metadata.serviceToken = "\(token.mid)"
                 if let id = Int(token.mid) {
                     lrc.metadata.artworkURL = URL(
@@ -149,12 +104,46 @@ extension LyricsProviders.QQMusic: _LyricsProvider {
             .eraseToAnyPublisher()
     }
 
-    public func _lyricsFetchPublisher(token: LyricsToken) -> AnyPublisher<Lyrics, Never> {
+
+    // MARK: - Another lyrics api, but without karaoke time info.
+
+    public func lyricsSearchPublisher2(request: LyricsSearchRequest) -> AnyPublisher<
+        LyricsToken, Never
+    > {
+        let parameter = [
+            "req_1": [
+                "method": "DoSearchForQQMusicDesktop",
+                "module": "music.search.SearchCgiService",
+                "param": [
+                    "num_per_page": "20",
+                    "page_num": "1",
+                    "query": request.searchTerm.titleOnly,
+                    "search_type": "0",
+                ],
+            ]
+        ]
+
+        let jsonData = try! JSONSerialization.data(withJSONObject: parameter, options: [])
+        let jsonString = String(data: jsonData, encoding: .utf8)!
+        let encodedJsonString = jsonString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        let urlString = qqSearchBaseURLString2 + "?data=" + encodedJsonString
+        let url = URL(string: urlString)!
+        return sharedURLSession.cx.dataTaskPublisher(for: url)
+            .map { $0.data }
+            .decode(type: QQResponseSearchResult2.self, decoder: JSONDecoder().cx)
+            .map(\.request.data.body.song.list)
+            .replaceError(with: [])
+            .flatMap(Publishers.Sequence.init)
+            .map { $0 as LyricsToken }
+            .eraseToAnyPublisher()
+    }
+
+    public func lyricsFetchPublisher2(token: LyricsToken) -> AnyPublisher<Lyrics, Never> {
         let parameter: [String: Any] = [
             "songmid": token.mid,
             "g_tk": 5381,
         ]
-        let url = URL(string: qqLyricsBaseURLString1 + "?" + parameter.stringFromHttpParameters)!
+        let url = URL(string: qqLyricsBaseURLString2 + "?" + parameter.stringFromHttpParameters)!
         var req = URLRequest(url: url)
         req.setValue("y.qq.com/portal/player.html", forHTTPHeaderField: "Referer")
         return sharedURLSession.cx.dataTaskPublisher(for: req)
@@ -175,9 +164,7 @@ extension LyricsProviders.QQMusic: _LyricsProvider {
 
                 lrc.idTags[.title] = token.name
                 lrc.idTags[.artist] = token.singers.joined(separator: ",")
-                //                lrc.idTags[.album] = token.albumname
-
-                //                lrc.length = Double(token.interval)
+               
                 lrc.metadata.serviceToken = "\(token.mid)"
                 if let id = Int(token.mid) {
                     lrc.metadata.artworkURL = URL(
